15e150a70b545333ff03422b32ccb757dfb6cf03
tloveday <thomas_loveday848@hotmail.com>
2021-08-25 15:11:59 +0000

    Add log out function

--- app.py
+++ app.py
@@ -86,6 +86,14 @@ def profile(username):
     return render_template("profile.html", username=username)
 
 
+@app.route("/logout")
+def logout():
+    # remove user from sessin cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
