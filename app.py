317aff3fc5c87f21e7738b70e3fd0778abbda17b
tloveday <thomas_loveday848@hotmail.com>
2021-08-27 18:41:29 +0000

    Add manage Categories Template

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def add_task():
         flash("Task Successfully Added")
         return redirect(url_for("get_tasks"))
 
-    categories = mongo.db.categories.find().sort("category_name", 1)
+    categories = mongo.db.Categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
 
 
@@ -135,7 +135,7 @@ def edit_task(task_id):
         flash("Task Successfully Updated")
 
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    categories = mongo.db.categories.find().sort("category_name", 1)
+    categories = mongo.db.Categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
 
 
@@ -144,6 +144,12 @@ def delete_task(task_id):
     mongo.db.tasks.remove({"_id": ObjectId(task_id)})
     flash("Task Successfully Deleted")
     return redirect(url_for("get_tasks"))
+
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.Categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
 
 
 if __name__ == "__main__":
